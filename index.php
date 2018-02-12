7b780b69d481f922f20bbeefe22bbc2f7db46d82
karimkhl <khlgeek@gmail.com>
2018-02-12 11:16:43 +0000

    Controller role

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
-require('model.php');
 
-$posts = getPosts();
+require('controller.php');
 
-require('indexView.php');
-
-
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'listPosts') {
+        listPosts();
+    }
+    elseif ($_GET['action'] == 'post') {
+        if (isset($_GET['id']) && $_GET['id'] > 0) {
+            post();
+        }
+        else {
+            echo 'Error : No post id detected.';
+        }
+    }
+}
+else {
+    listPosts();
+}
 
 
