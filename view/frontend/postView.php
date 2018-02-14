45c129c89a3309a7e7d0fae4739b3ce37206aa92
karimkhl <khlgeek@gmail.com>
2018-02-14 00:01:47 +0000

    ..

--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -32,5 +32,18 @@
             <?php
         }
         ?>
+        <form action="index.php?action=addComment&amp;id=<?= $post['id'] ?>" method="post">
+            <div>
+                <label for="author">Auteur</label><br />
+                <input type="text" id="author" name="author" />
+            </div>
+            <div>
+                <label for="comment">Commentaire</label><br />
+                <textarea id="comment" name="comment"></textarea>
+            </div>
+            <div>
+                <input type="submit" />
+            </div>
+        </form>
     </body>
 </html>
