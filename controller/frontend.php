a7d0b21e1fcade4b76f6969a31f48199e27d38bd
karimkhl <khlgeek@gmail.com>
2018-02-14 10:34:36 +0000

    Transition of model to object :  - update controller

--- controller/frontend.php
+++ controller/frontend.php
@@ -10,22 +10,27 @@ require(__DIR__ .'/../model/frontend.php');
 
 function listPosts()
 {
-    $posts = getPosts();
+    $postManager = new PostManager();
+    $posts = $postManager->getPosts();
 
     require(__DIR__ .'/../view/frontend/listPostsView.php');
 }
 
 function post()
 {
-    $post = getPost($_GET['id']);
-    $comments = getComments($_GET['id']);
+    $postManager = new PostManager();
+    $post = $postManager->getPost($_GET['id']);
+
+    $commentManager = new CommentManger();
+    $comments = $commentManager->getComments($_GET['id']);
 
     require(__DIR__ .'/../view/frontend/postView.php');
 }
 
 function addComment($postId, $author, $comment)
 {
-    $affectedLines = postComment($postId, $author, $comment);
+    $commentManager = new CommentManger();
+    $affectedLines = $commentManager->postComment($postId, $author, $comment);
 
     if ($affectedLines === false) {
         throw new Exception('Impossible d\'ajouter le commentaire !');
